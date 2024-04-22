a9fcfc6b059e8d4f6e39a4a2f956be38acc4804b
Sanija <sanijasokirka@gmail.com>
2024-04-22 09:41:26 +0300

    fsdsdfsd

--- registracija.py
+++ registracija.py
@@ -27,9 +27,19 @@ layout=[
     [sg.T("Vārds:"), sg.InputText(key="vards")],
     [sg.T("Uzvārds:"), sg.InputText(key="uzvards")],
     [sg.T("Dzimums:"), sg.Radio("Vīrietis","dzimums", key="dzimumsV"), sg.Radio("Sieviete","dzimums", key="dzimumsS")], # "dzimums"- jeb izveidotā grupa apvienos visas radio pogas
-    [sg.T("Vecums:"), sg.Radio(key="radio")],
-
-]
-#galvenais cikls
-#viss... jaizver
+    [sg.T("Vecums:"), sg.Combo([i for i in range(18,101)], key="vecums")],
+    [sg.T("Ierašanās datums:"), sg.CalendarButton("Izvēlēties datumu", target="ierasanas_datums", key="ierasanas_datums")], # poas nosaukums(calander)), ...., atslēga
+    [sg.Button("Reģistrēties"), sg.Button("Iziet")]
+    ]
 logs=sg.Window("Datu reģistrs", layout, resizable=True)
+
+#galvenais cikls
+
+while True:
+ event, values=logs.read()
+ if event==sg.WIN_CLOSED or event== "Iziet":
+  break
+ 
+#viss... jaizver
+
+ logs.close()
